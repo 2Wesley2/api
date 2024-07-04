d3567588f87f0cea965254372c17cfe282e6fbaa
2Wesley2 <wesley.fer.almeida98@gmail.com>
2024-07-04 18:34:47 -0300

    refactor: Update retailer registration with role permission validation
    - Add validateRolePermission function to validate role permissions using RolePermission
    - Use process.env.RETAILER_ROLE_ID to get the retailer role ID
    - Improve error handling by returning specific messages and status codes
    - Adjust RolePermission lookup by role: retailerRoleId

--- src/controllers/userControllers/registerRetailer.js
+++ src/controllers/userControllers/registerRetailer.js
@@ -2,20 +2,44 @@ const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const { createPerson } = require('../personController');
 const RolePermission = require('../../models/RolePermission');
+require('dotenv').config();
+
+const validateRolePermission = async () => {
+  const errors = [];
+  const retailerRoleId = process.env.RETAILER_ROLE_ID;
+
+  if (!retailerRoleId) {
+    errors.push({ msg: 'Role ID not configured', status: 500 });
+  } else {
+    const retailerRolePermission = await RolePermission.findOne({
+      role: retailerRoleId,
+    }).lean();
+    if (!retailerRolePermission) {
+      errors.push({ msg: 'RolePermission not found', status: 404 });
+    }
+  }
+
+  return errors.filter(Boolean);
+};
 
 exports.registerRetailer = async (req, res) => {
   const { email, password, phone, cpf, firstName, lastName, birthDate } =
     req.body;
 
   try {
-    const retailerRolePermission = await RolePermission.findOne({
-      'role.roleName': 'retailer',
-    }).lean();
+    const errors = await validateRolePermission();
 
-    if (!retailerRolePermission) {
-      return res.status(400).json({ msg: 'Role not found' });
+    if (errors.length > 0) {
+      // Retorna todas as mensagens de erro e os status correspondentes
+      const highestStatus = errors.reduce(
+        (highest, error) => Math.max(highest, error.status),
+        400,
+      );
+      return res.status(highestStatus).json({
+        messages: errors.map((e) => e.msg),
+        statusCodes: errors.map((e) => e.status),
+      });
     }
-
     const personId = await createPerson({
       cpf,
       firstName,
@@ -30,7 +54,7 @@ exports.registerRetailer = async (req, res) => {
       password: hashedPassword,
       phone,
       person: personId,
-      rolePermission: retailerRolePermission._id,
+      rolePermission: process.env.RETAILER_ROLE_ID,
     });
 
     await newUser.save();
